e86f90b25780d043df6cd82f23d795719774f190
E Abella <eabella@Es-MacBook-Air.local>
2024-07-30 10:00:24 -0700

    testing commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 //import Card from '../components/cards/Card';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+
 
 const Home = () => {
+  const navigate = useNavigate(); 
+
+  const handleButtonClick = () => {
+    navigate('/recipe-form'); // Navigate to RecipeForm page
+  };
+
   return (
     <>
       <br/>
       <h2>Welcome to the recipe app! </h2>
       <br/>
-      <button style={{ color: 'white'}}> Create Your Recipe!</button>
+      <button style={{ color: 'white'}} className="button-style" onClick={handleButtonClick}> Create Your Recipe!</button>
       {/* <Card /> */}
     </>
   );
